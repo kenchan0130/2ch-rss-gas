b20dc7950aaaf391a127fce20839b03d8479d469
Tadayuki Onishi <tonishi@folio-sec.com>
2019-01-21 13:04:26 +0900

    fix build settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,51 @@ const path = require('path');
 const GasPlugin = require('gas-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // disable minify for bundled file
-  devtool: 'inline-source-map',
-  context: __dirname,
-  entry: {
-    main: path.resolve(__dirname, 'src', 'index.ts'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'Code.js',
-  },
-  resolve: {
-    extensions: [
-      '.ts',
-      '.tsx',
-      '.js'
-    ],
-    // modules: [path.resolve(__dirname, '/src'), 'node_modules']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-      },
-      {
-        test: /\.js$/,
-        // include: /\/node_modules\//,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                "targets": {
-                  "browsers": ["ie 8"]
+    mode: 'development', // disable minify for bundled file
+    devtool: 'inline-source-map',
+    context: __dirname,
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'Code.js',
+    },
+    resolve: {
+        extensions: [
+            '.ts',
+            '.tsx',
+            '.js'
+        ],
+    },
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        [
+                            'gas'
+                        ],
+                        [
+                            "@babel/preset-env",
+                            {
+                                "targets": {
+                                    "browsers": ["ie 8"]
+                                }
+                            }
+                        ],
+                    ],
                 }
-              }
-            ]
-          ],
-        }
-      }
+            }
+        ],
+    },
+    plugins: [
+        new GasPlugin(),
     ],
-  },
-  plugins: [
-    new GasPlugin(),
-  ],
 };
